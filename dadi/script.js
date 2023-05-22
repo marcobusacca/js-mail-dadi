a5fa2d59bfba49584f9b56fa7ac44804764441c0
marcobusacca <marcobusacca01@gmail.com>
2023-05-22 17:18:27 +0200

    [JS-DADI] FIX

--- dadi/script.js
+++ dadi/script.js
@@ -1,25 +1,24 @@
 "use strict";
 
-// // ALERT DI BENVENUTO
-// alert(`Benvenuto! Questa applicazione si chiama "Gioco Dei Dadi": 
+// ALERT DI BENVENUTO
+alert(`Benvenuto! Questa applicazione si chiama "Gioco Dei Dadi": 
 
-// Generemo per te e per il Computer (il tuo avversario) un Numero Random da 1 a 6.
-
-
-// Se il Tuo Numero è MAGGIORE rispetto al Numero del Computer VINCI!
-
-// Se il Tuo Numero è MINORE rispetto al Numero del Computer PERDI!
-
-// Se entrambi i Numeri sono UGUALI, PAREGGIATE!
+Generemo per te e per il Computer (il tuo avversario) un Numero Random da 1 a 6.
 
 
-// Clicca su "OK" per Incominciare a Giocare!`);
+Se il Tuo Numero è MAGGIORE rispetto al Numero del Computer VINCI!
+
+Se il Tuo Numero è MINORE rispetto al Numero del Computer PERDI!
+
+Se entrambi i Numeri sono UGUALI, PAREGGIATE!
+
+
+Clicca su "OK" per Incominciare a Giocare!`);
 
 // NUMERO RANDOM UTENTE
 const user_number = Math.floor(Math.random() * (7-1) + 1);
-console.log(user_number);
-
+alert(`Il Tuo Numero è: ${user_number}`);
 
 // NUMERO RANDOM COMPUTER
 const computer_number = Math.floor(Math.random() * (7-1) + 1);
-console.log(computer_number);
+alert(`Il Numero del Computer è: ${computer_number}`);
