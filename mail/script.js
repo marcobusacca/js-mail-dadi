35961f14b2267838a69c473c889d3bbea72140d5
marcobusacca <marcobusacca01@gmail.com>
2023-05-22 14:58:58 +0200

    [JS-MAIL] Creazione Prompt Richiesta Email Utente

--- mail/script.js
+++ mail/script.js
@@ -2,3 +2,6 @@
 
 // ARRAY LISTA MAIL CON PERMESSO DI ACCESSO
 let mail_list = ["luca@gmail.com", "marco@gmail.com", "paolo@gmail.com"];
+
+// PROMPT RICHIESTA EMAIL ALL'UTENTE
+const user_mail = prompt('Inserisci la tua email:');
