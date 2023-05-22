263e59c6640e0ff0496db9324f5363999bd1207a
marcobusacca <marcobusacca01@gmail.com>
2023-05-22 17:10:40 +0200

    [JS-DADI] Alert di Benvenuto

--- dadi/script.js
+++ dadi/script.js
@@ -1,1 +1,16 @@
 "use strict";
+
+// ALERT DI BENVENUTO
+alert(`Benvenuto! Questa applicazione si chiama "Gioco Dei Dadi": 
+
+Generemo per te e per il Computer (il tuo avversario) un Numero Random da 1 a 6.
+
+
+Se il Tuo Numero è MAGGIORE rispetto al Numero del Computer VINCI!
+
+Se il Tuo Numero è MINORE rispetto al Numero del Computer PERDI!
+
+Se entrambi i Numeri sono UGUALI, PAREGGIATE!
+
+
+Clicca su "OK" per Incominciare a Giocare!`);
